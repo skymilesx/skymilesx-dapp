4d71d59aa06ce2e26210d43fb89ebd790bda4bc5
skymilesx <RAMI.fayek@icloud.com>
2025-04-11 18:46:37 +0300

    Update skyxconfig.ts

--- skyxconfig.ts
+++ skyxconfig.ts
@@ -1,5 +1,5 @@
-// src/skyxConfig.ts
-import ABI from './contracts/SKYXToken_ABI.json';
-
-export const SKYX_ADDRESS = "0xf8289Fe3506F28efb906a0bEf1F140665CbDD375"; // Your deployed MegaETH contract
-export const SKYX_ABI = ABI;
+export const SKYX_ADDRESS = "0xf8289Fe3506F28efb906a0bEf1F140665CbDD375";  // Your contract address after deploying
+export const SKYX_ABI = [
+  // ABI of the contract
+  // You can obtain this from Remix or Etherscan (contract verification)
+];
